e9ccfbccf0013f4764ebbff30e75619bf4b6704d
scohe001 <scohe001@ucr.edu>
2016-01-27 07:29:54 +0000

    Added main skeleton for students

---
+++ main_outline.cpp
@@ -0,0 +1,21 @@
+//includes
+
+int main() {
+    //Create Player objects
+    //Create Board object
+    
+    //set active player
+    while (/*Game is still being played*/) {
+        //display board state
+        
+        //get move (make sure it's valid!)
+        
+        //make move
+        
+        //switch active player
+    }
+    
+    //Display final board state and game outcome
+    
+    return 0;
+}
